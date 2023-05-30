0046cd4d19f2362d984350ae798f42ede3c9fa77
Alessandro L <aflinsjr@icloud.com>
2023-05-30 19:34:24 -0300

    react vite ts finished

--- src/App.tsx
+++ src/App.tsx
@@ -9,48 +9,51 @@ function App() {
 
   return (
     <div className="container">
-      <div className="container-login">
-        <form className="login-form">
-
-          <span className="login-form-title">Bem Vindo!</span>
-          <span className="login-form-title">
-            <img src={jpIMG} alt="Alessandro Dev." />
-          </span>
-
-          <div className='wrap-input'>
-            <input 
-              className='input'
-              type="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-            <span className='focus-input' data-placeholder='Email'></span>
-          </div>
-
-          <div className='wrap-input'>
-            <input 
-              className='input'
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              />
-              <span className='focus-input' data-placeholder='Password'></span>
-          </div>
-
-          <div className="container-login-form-btn">
-            <button className="login-form-btn">Login</button>
-          </div>
-
-          <div className="text center">
-            <span className="text1">Não possui conta?</span>
-
-            <a className='txt2' href="#">Criar conta.</a>
-          </div>
-        </form>
-      </div>
       <GlobalStyle />
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title"> Bem vindo </span>
+
+            <span className="login-form-title">
+              <img src={jpIMG} alt="Jovem Programador" />
+            </span>
+
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta? </span>
+              <a className="txt2" href="#">
+                Criar conta
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
