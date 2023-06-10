e99cd108173296d2699d22d78843b113b300606b
SnorlaxSPI <aflinsjr@icloud.com>
2023-06-10 18:21:44 -0300

    Criando tela de cadastro pt1

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login } from "../pages/Login";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path = '/' element = {<Login />} />
+      </Routes>
+    </Router>
+  )
+}
